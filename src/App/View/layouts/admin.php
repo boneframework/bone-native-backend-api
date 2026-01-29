07cf86d1867999973873d99c279d8e32fd2b94ad
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-01-29 19:45:19 +0100

    update packages, config, layout

--- src/App/View/layouts/admin.php
+++ src/App/View/layouts/admin.php
@@ -1,10 +1,10 @@
 <?php
+/** @var \Del\Entity\User $user */
+/** @var string $content */
+
 use Del\Cdn;
 use Del\Icon;
 use Del\Image;
-/**
- * @var \Del\Entity\User $user
- */
 $person = $user->getPerson();
 $image = $person->getImage() ? new Image('data/uploads/' . $person->getImage()) : null;
 
@@ -70,7 +70,22 @@ $image = $person->getImage() ? new Image('data/uploads/' . $person->getImage()) 
         <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="fa fa-gear"></i>
+                    <?php if ($image) { ?> 
+                        <img src="<?= $image->outputBase64Src() ?>" class="img-circle img-size-32" alt="User Image">
+                    <?php } ?>
+                    
+                    <span class="mx-1">
+                        <?php
+                        $name = $user->getEmail();
+
+                        if ($person->getFirstname()) {
+                            $name = $person->getLastname() ? $person->getFirstname() . ' ' . $person->getLastname() : $person->getFirstname();
+                        }
+
+                        echo $name;
+                        ?>
+                    </span>
+                    <i class="fa fa-caret-down"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                     <a href="/user/edit-profile" class="dropdown-item text-center text-muted">
@@ -93,36 +108,14 @@ $image = $person->getImage() ? new Image('data/uploads/' . $person->getImage()) 
 
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <a href="/user/home" class="brand-link">
-            <img src="/img/skull_and_crossbones.png" alt="AdminLTE Logo" class="brand-image "
+            <img src="/img/skull_and_crossbones.png" alt="Bone Framework" class="brand-image "
                  style="opacity: .8">
             <span class="brand-text font-weight-light">Administration</span>
         </a>
 
         <div class="sidebar">
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="<?= $image ? $image->outputBase64Src() : '' ?>" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="/user/home" class="d-block"><?php
-                        $name = $user->getEmail();
-                        if ($person->getFirstname()) {
-                            $name = $person->getLastname() ? $person->getFirstname() . ' ' . $person->getLastname() : $person->getFirstname();
-                        }
-                        echo $name;
-                        ?></a>
-                </div>
-            </div>
-
             <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <li class="nav-item">
-                        <a href="/admin/somewhere" class="nav-link">
-                            <i class="nav-icon fa fa-calendar"></i>
-                            <p>Some Admin Link!</p>
-                        </a>
-                    </li>
-                </ul>
+                <?= $this->adminLinks() ?>
             </nav>
         </div>
     </aside>
@@ -137,7 +130,7 @@ $image = $person->getImage() ? new Image('data/uploads/' . $person->getImage()) 
             <br>
             <?php /** @todo get vars in here */ ?>
             <p><?= Icon::HOME ?>  Address here | <?= Icon::PHONE ?> phone here | <?= Icon::ENVELOPE ?> <a href="mailto:emailhere">emailhere</a>.</p>
-            <p>© 2014 <?= date('Y') > 2014 ? ' - ' . date('Y') : '' ?> sitename | Site created by <a target="_blank" href="//authorurl">author</a></p>
+            <p>© 2020 <?= date('Y') > 2020 ? ' - ' . date('Y') : '' ?> sitename | Site created by <a target="_blank" href="//authorurl">author</a></p>
         </div>
     </footer>
 
